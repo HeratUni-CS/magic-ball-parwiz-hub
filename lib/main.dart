0a56e357ac3e0b057875528d4de3248fc6dd4fe1
parwiz-hub <parwizyosefi786@gmail.com>
2023-05-02 10:37:07 -0700

    Ball Created

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: null,
+     MaterialApp(
+      home: BallPage(),
     ),
   );
+}
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Container(),
+    );
+  }
 }
