cb8391592e0449f83e06921dbab466187841a62c
parwiz-hub <parwizyosefi786@gmail.com>
2023-05-02 10:52:43 -0700

    Randomized

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-     MaterialApp(
+    MaterialApp(
       home: BallPage(),
     ),
   );
 }
+
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -16,7 +19,7 @@ class BallPage extends StatelessWidget {
         backgroundColor: Colors.blue[900],
         title: Text('Ask Me Anything'),
       ),
-      body:Ball(),
+      body: Ball(),
     );
   }
 }
@@ -28,12 +31,16 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Center(
       child: TextButton(
         onPressed: () {
-          print('I got clicked');
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+          print('I got clicked : $ballNumber');
         },
         child: Image.asset('images/ball1.png'),
       ),
